2c35cd234c428a3f4672a8ce51b0c815572dc31d
xieyuxing <185246042@qq.com>
2018-07-19 14:58:38 +0800

    init

--- src/main/java/com/lingyun/generics/Interval.java
+++ src/main/java/com/lingyun/generics/Interval.java
@@ -1,7 +1,16 @@
 package com.lingyun.generics;
 
-public class Interval<T extends Comparable> extends Pair<T>{
-    public T getMax(){
+public class Interval<Q extends Comparable,S> extends Pair<Q>{
+    public Q getMax(){
         return getFirst().compareTo(getSecond())>0?getFirst():getSecond();
+    }
+    private S s;
+
+    public S getS() {
+        return s;
+    }
+
+    public void setS(S s) {
+        this.s = s;
     }
 }
