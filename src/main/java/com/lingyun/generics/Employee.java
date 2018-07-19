df97b13f38307077324f8df24cf88d2eb4b8d9b8
xieyuxing <185246042@qq.com>
2018-07-19 17:28:44 +0800

    init

--- src/main/java/com/lingyun/generics/Employee.java
+++ src/main/java/com/lingyun/generics/Employee.java
@@ -1,7 +1,17 @@
 package com.lingyun.generics;
 
 public class Employee implements Comparable<Employee>{
+
+    private String name;
     private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public double getSalary() {
         return salary;
@@ -16,5 +26,14 @@ public class Employee implements Comparable<Employee>{
         if (salary>o.salary) return 1;
         if (salary<o.salary) return -1;
         return 0;
+    }
+    public static void printBuddies(Pair<Employee> p){
+        Employee first=p.getFirst();
+        Employee second=p.getSecond();
+        System.out.println(first.getName()+ " and "+second.getName());
+    }
+    public static void main(String[] args){
+        Pair<Manager> managerPair=new Pair<>();
+//        printBuddies(managerPair);//error
     }
 }
