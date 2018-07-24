8969ed4b26e6bf2aff6b1db399522a89ad4917fa
xieyuxing <185246042@qq.com>
2018-07-24 14:38:54 +0800

    init

--- src/main/java/com/lingyun/generics/SuperTypeWildCards.java
+++ src/main/java/com/lingyun/generics/SuperTypeWildCards.java
@@ -1,5 +1,6 @@
 package com.lingyun.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SuperTypeWildCards {
@@ -8,5 +9,17 @@ public class SuperTypeWildCards {
         apples.add(new Jonathan());
         //apples.add(new Fruit());//Error
         Object s=apples.get(0);
+    }
+    public void usefulWriteTo(List<? super Fruit> fruits){
+        fruits.add(new Apple());
+        fruits.add(new Jonathan());
+        fruits.add(new Fruit());
+        Object s=fruits.get(0);
+    }
+    public static void main(String[] args){
+        SuperTypeWildCards a = new SuperTypeWildCards();
+        //List<? super  Fruit> fruits=new ArrayList<Apple>();
+        List<? super  Fruit> fruits=new ArrayList<Fruit>();
+        a.usefulWriteTo(fruits);
     }
 }
