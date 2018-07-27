9d75a742cf2e44b21c496f14ce26f341c0afd751
xieyuxing <185246042@qq.com>
2018-07-27 09:50:09 +0800

    init

--- src/main/java/com/lingyun/generics/CaptureConversion.java
+++ src/main/java/com/lingyun/generics/CaptureConversion.java
@@ -9,22 +9,32 @@ public class CaptureConversion {
     static void f2(GoodHolder<?> holder){
         f1(holder);//call with captured type
     }
+    static void f3(GoodHolder holder){
+        f1(holder);
+    }
     @SuppressWarnings("unchecked")
     public static void main(String[] args){
         GoodHolder raw = new GoodHolder<Integer>(1);
         f1(raw);//Produces warnings
         f2(raw);//No warnings
+        f3(raw);
         GoodHolder rawBasic=new GoodHolder();
         rawBasic.set(new Apple());//Warning
+        f1(rawBasic);
         f2(rawBasic);//No warnings
+        f3(rawBasic);
         //Upcast to GoodHolder<?>,still figures it out:
         GoodHolder<?> wildcarded=new GoodHolder<Double>(1.0);
         f2(wildcarded);
+        f3(wildcarded);
     }
 }
 /*Output:
 Integer
 Integer
+Integer
 Apple
+Apple
+Double
 Double
  */
